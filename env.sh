6cc8b31faea959013a769a35860153ded7f5bdf1
MAX1T1A <max1t1aldan@gmail.com>
2025-11-01 17:15:41 +0300

    commit

---
+++ env.sh
@@ -0,0 +1,8 @@
+export TELEGRAM_BOT_TOKEN="BOT_TOKEN"
+export TELEGRAM_CHAT_ID=111111111
+
+export WHATSAPP_CHAT_JIDS="121231421421@s.whatsapp.net"
+# export WHATSAPP_CHAT_JIDS="" # если нужно читать из всех чатов
+
+export TRIGGER_TEXTS="слова,на,которые,скрипт,должен,реагировать"
+# export TRIGGER_TEXTS="" # если нужно реагировать на все
